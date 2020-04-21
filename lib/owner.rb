35ce7f9589ba09cf26f1a4eb632392fe14e61635
Victor Plushko <vicplushko33@gmail.com>
2020-04-21 03:13:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,55 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+ 
+ @@all = []
+ 
+ def initialize(name)
+   @name = name
+   @species= "human"
+   save
+ end
+ 
+ def save
+   @@all << self
+ end
+ 
+ def say_species
+  "I am a #{species}."
+ end
+ 
+ def cats
+   Cat.all.select {|kitty| kitty.owner == self }
+ end
+ 
+ def dogs
+   Dog.all.select {|pup| pup.owner == self}
+ end
+ 
+ def buy_cat(name)
+   Cat.new(name, self)
+ end
+ 
+ def buy_dog(name)
+   Dog.new(name, self)
+ end
+ 
+ def walk_dogs
+   self.dogs.each {|pups| pups.mood = "happy"}
+ end
+ 
+ def feed_cats
+   self.cats.each {|kitty| kitty.mood = "happy"}
+ end
+ 
+ def self.all
+  @@all
+ end
+ 
+ def self.count
+   @@all.count
+ end
+ 
+ def self.reset_all
+   @@all.clear
+ end
 end
